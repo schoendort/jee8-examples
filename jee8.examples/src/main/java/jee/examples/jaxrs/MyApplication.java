77f290d8ebcc06e86667f0937559e918f050369d
schoendort <tschoendorfer92@gmail.com>
2017-11-08 20:34:21 +0100

    update

--- jee8.examples/src/main/java/jee/examples/jaxrs/MyApplication.java
+++ jee8.examples/src/main/java/jee/examples/jaxrs/MyApplication.java
@@ -12,6 +12,7 @@ public class MyApplication extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> resources = new HashSet<>();
+		resources.add(MyResource.class);
 		return resources;
 	}
 }
